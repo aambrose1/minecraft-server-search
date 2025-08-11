9da22077c69783ac037328a8ef8f4644a0abc32f
Andrea Ambrose <andreamoesha@gmail.com>
2025-08-10 20:54:37 -0500

    link api to search

---
+++ src/components/Search.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+const BASE_URL = "https://api.mcsrvstat.us/3/";
+const OPTIONS = {
+  method: 'GET', 
+  headers: {
+    accept: 'application/json'
+  }
+};
+
+const sample_address = "hypixel.net"
+
+export default function Search(){
+    // const [address, setAddress] = useState("");
+    const [data, setData] = useState({});
+
+    const fetchData = async () => {
+        try{
+            const response = await fetch(BASE_URL+sample_address, OPTIONS);
+            const data:Object = await response.json();
+            setData(data);
+            console.log(data);
+        }
+        catch (e){
+            console.log(e);
+        }
+    }
+
+    useEffect(() => {
+        fetchData();
+    },[]);
+
+    return(
+        <p>{data["ip"]}</p>
+    )
+}
+
+
